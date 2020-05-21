dfe270e6a48f96215087bc31714fa41f8672f1fa
Nikita <mynewacc.create@gmail.com>
2020-05-21 19:40:39 +0300

    feat: implement StartupServlet servlet and AddUser class

---
+++ Ad/src/tag/AddUser.java
@@ -0,0 +1,44 @@
+package tag;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import entity.User;
+import entity.UserList;
+import entity.UserList.UserExistsException;
+import helper.UserListHelper;
+
+public class AddUser extends SimpleTagSupport {
+	
+	private User user;
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+    
+	public void doTag() throws JspException, IOException {
+		// Изначально описание ошибки = null (т.е. ошибки нет)
+		String errorMessage = null;
+		// Извлечь из контекста приложения общий список пользователей
+		UserList userList = (UserList) getJspContext().getAttribute("users", PageContext.APPLICATION_SCOPE);
+		if (user.getLogin() == null || user.getLogin().equals("")) {
+			errorMessage = "Логин не может быть пустым!";
+		} else {
+			if (user.getName() == null || user.getName().equals("")) {
+				errorMessage = "Имя пользователя не может быть пустым!";
+			}
+		}
+		if (errorMessage == null) {
+			try {
+				userList.addUser(user);
+				UserListHelper.saveUserList(userList);
+			} catch (UserExistsException e) {
+				errorMessage = "Пользователь с таким логином уже существует!";
+			}
+		}
+		// Сохранить описание ошибки (текст или null) в сессии
+		getJspContext().setAttribute("errorMessage", errorMessage,
+				PageContext.SESSION_SCOPE);
+	}
+}
